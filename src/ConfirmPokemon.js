95c8b84dcb097764b78da3def86ad96c4ec82212
HendersonTyler <henderson.tyler@outlook.com>
2019-09-17 16:41:13 +0900

    Moved more fuctions into useEffect

--- src/ConfirmPokemon.js
+++ src/ConfirmPokemon.js
@@ -5,54 +5,55 @@ import Pokeball from './pokeball.gif';
 const ConfirmPokemon = ({ yourPokemon, setyourPokemonData, yourPokemonData, yourWeakness, yourTypes, setyourWeakness, setyourTypes, setyourStrength, yourStrength, setgameScore, setroute }) => {
 
     useEffect(() => {
+        const storePokemonData = async () => {
+            setyourPokemonData('');
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${yourPokemon}`);
+            if (response.status === 200 && !isEmptyOrSpaces(yourPokemon)) {
+                const data = await response.json();
+                setyourPokemonData(data);
+                setyourTypes(data.types);
+                type(data.types);
+            } else {
+                alert("Sorry, we couldn't find that pokemon. Please try again.");
+            }
+        };
         storePokemonData();
-    }, []);
+        const type = async (chosenPokeType) => {
+            let h = [];
+            let w = [];
+
+            for (let i = 0; i < chosenPokeType.length; i++) {
+                const response = await fetch(`https://pokeapi.co/api/v2/type/${chosenPokeType[i].type.name}`)
+                if (response.status === 200) {
+                    if (response.status === 200) {
+                        const data = await response.json();
+                        let z = data.damage_relations.double_damage_from;
+                        let b = data.damage_relations.double_damage_to;
+                        for (let q = 0; q < z.length; q++) {
+                            h.push(z[q].name);
+                        }
+                        for (let l = 0; l < b.length; l++) {
+                            w.push(b[l].name);
+
+                        }
+                    } else {
+                        alert("Sorry, we couldn't find the stats");
+                    }
+                }
+            }
+            setyourWeakness(h);
+            setyourStrength(w);
+            setgameScore(0);
+        };
+    }, [setyourPokemonData, setyourTypes, yourPokemon, setgameScore, setyourStrength, setyourWeakness]);
 
     function isEmptyOrSpaces(str) {
         return str === null || str.match(/^ *$/) !== null;
     }
 
-    const storePokemonData = async () => {
-        setyourPokemonData('');
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${yourPokemon}`);
-        if (response.status === 200 && !isEmptyOrSpaces(yourPokemon)) {
-            const data = await response.json();
-            setyourPokemonData(data);
-            setyourTypes(data.types);
-            type(data.types);
 
-        } else {
-            alert("Sorry, we couldn't find that pokemon. Please try again.");
-        }
-    };
 
-    const type = async (chosenPokeType) => {
-        let h = [];
-        let w = [];
 
-        for (let i = 0; i < chosenPokeType.length; i++) {
-            const response = await fetch(`https://pokeapi.co/api/v2/type/${chosenPokeType[i].type.name}`)
-            if (response.status === 200) {
-                if (response.status === 200) {
-                    const data = await response.json();
-                    let z = data.damage_relations.double_damage_from;
-                    let b = data.damage_relations.double_damage_to;
-                    for (let q = 0; q < z.length; q++) {
-                        h.push(z[q].name);
-                    }
-                    for (let l = 0; l < b.length; l++) {
-                        w.push(b[l].name);
-
-                    }
-                } else {
-                    alert("Sorry, we couldn't find the stats");
-                }
-            }
-        }
-        setyourWeakness(h);
-        setyourStrength(w);
-        setgameScore(0);
-    };
 
     return (
         <div className="container">
